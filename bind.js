d0ead0f8336ffa51b41b769174ec7bae7b0cef3f
CarterLi <tongzhou.li@ele.me>
2015-12-26 15:27:31 +0800

    Init support deep property binding
    This disables Firefox support

--- bind.js
+++ bind.js
@@ -1,92 +1,126 @@
 'use strict';
 
-function pushOrSet(object, key, value) {
-  if (key in object) {
-    object[key].push(value);
-  } else {
-    object[key] = [value];
+class Binder {
+  static pushOrSet(object, key, value) {
+    if (key in object) {
+      object[key].push(value);
+    } else {
+      object[key] = [value];
+    }
   }
-}
 
-function setTwowayBinding(elem, vm, expText, bindings) {
-  vm[expText] = elem.value;
-
-  elem.addEventListener('input', evt => {
+  setTwowayBinding(elem, vm, expText) {
     vm[expText] = elem.value;
-  });
-  
-  pushOrSet(bindings, expText, function expression(vm) {
-    elem.value = vm[expText];
-  });
-}
 
-function setEventBinding(elem, vm, eventName, expText) {
-  elem.addEventListener(eventName, function expression($event) {
-    try {
-      return this.call(vm, $event);
-    } catch(e) {
-      if (e instanceof SyntaxError) throw e;
-    }
-  }.bind(new Function('$event', expText)))
-}
+    elem.addEventListener('input', () => {
+      vm[expText] = elem.value;
+    });
 
-function setOnewayBinding(elem, vm, property, expText, bindings) {
-  const expression = function expression(property, vm) {
-    try {
-      elem[property] = this.call(vm);
-    } catch(e) {
-      if (e instanceof SyntaxError) throw e;
-      elem[property] = '';
-    }
-  }.bind(new Function('return ' + expText), property);
-
-  expression(vm);
-
-  for (var match, regexp = /this\.(\w*)/g; (match = regexp.exec(expText)); ) {
-    pushOrSet(bindings, match[1], expression);
+    Binder.pushOrSet(this.bindings, expText, function expression(vm) {
+      elem.value = vm[expText];
+    });
   }
-}
 
-function startWatching(vm, bindings) {
-  function updateProperty(obj, prop) {
-    const bindingExpressions = bindings[prop];
+  setEventBinding(elem, vm, eventName, expText) {
+    elem.addEventListener(eventName, function expression($event) {
+      try {
+        return this.call(vm, $event);
+      } catch (e) {
+        if (e instanceof SyntaxError) throw e;
+      }
+    }.bind(new Function('$event', expText)));
+  }
+
+  setOnewayBinding(elem, vm, property, expText) {
+    const expression = function expression(property, vm) {
+      try {
+        elem[property] = this.call(vm);
+      } catch (e) {
+        if (e instanceof SyntaxError) throw e;
+        elem[property] = '';
+      }
+    }.bind(new Function('return ' + expText), property);
+
+    expression(vm);
+
+    for (let match, regexp = /this(?:\.\w+)+/g; (match = regexp.exec(expText)); ) {
+      Binder.pushOrSet(this.bindings, match[0].substr(5), expression);
+    }
+  }
+
+  updateProperty(obj, prop) {
+    const bindingExpressions = this.bindings[prop];
     if (bindingExpressions) {
       bindingExpressions.forEach(expression => expression(obj));
     }
   }
 
-  if (typeof Proxy !== 'undefined') {
-    return new Proxy(vm, {
-      set(obj, prop, value) {
-        obj[prop] = value;
-        updateProperty(obj, prop);
-        return true;
+  startWatching(vm, prefix) {
+    if (Array.isArray(vm)) {
+      throw new Error('Array is not supported (yet)');
+    } else {
+      for (const key in vm) {
+        if (vm.hasOwnProperty(key)) {
+          const value = vm[key];
+          if (value != null && typeof value === 'object') {
+            this.startWatching(value, `${prefix}${key}.`);
+          }
+        }
       }
+    }
+
+    return this.observeObject(vm, prefix);
+  }
+
+  setupEssential() {
+    if (typeof Proxy !== 'undefined') {
+      this.observeObject = function observeObject(vm, prefix) {
+        const that = this;
+        return new Proxy(vm, {
+          set(obj, prop, value) {
+            if (obj[prop] !== value) {
+              obj[prop] = value;
+              that.updateProperty(obj, prefix + prop);
+            }
+            return true;
+          }
+        });
+      };
+    } else if (Object.observe) {
+      this.observeObject = function observeObject(vm, prefix) {
+        Object.observe(vm, changes => {
+          changes.forEach(change => this.updateProperty(change.object, prefix + change.name));
+        });
+        return vm;
+      };
+    } else {
+      throw new Error('Unsupported browser');
+    }
+  }
+
+  constructor(root, vm) {
+    this.setupEssential();
+    this.bindings = Object.create(null);
+    vm = this.startWatching(vm, '');
+
+    Array.prototype.forEach.call(root.querySelectorAll('*'), elem => {
+      Object.keys(elem.dataset).forEach(key => {
+        if (key === 'init') {
+          new Function(elem.dataset[key]).call(vm);
+        } else if (key === 'model') {
+          this.setTwowayBinding(elem, vm, elem.dataset[key]);
+        } else if (/^on[A-Z]/.test(key)) {
+          this.setEventBinding(elem, vm, key.slice(2).toLowerCase(), elem.dataset[key]);
+        } else {
+          this.setOnewayBinding(elem, vm, key === 'bind' ? 'textContent' : key, elem.dataset[key]);
+        }
+      });
     });
-  } else if (Object.observe) {
-    Object.observe(vm, changes => {
-      changes.forEach(change => updateProperty(change.object, change.name));
-    });
-    return vm;
-  } else {
-    throw new Error('Unsupported browser');
+
+    this.viewModel = vm;
   }
 }
 
-function bootstrap(root, vm) {
-  const bindings = Object.create(null);
-  
-  Array.prototype.forEach.call(root.querySelectorAll('*'), elem => {
-    Object.keys(elem.dataset).forEach(key => {
-      if (key === 'model') {
-        setTwowayBinding(elem, vm, elem.dataset[key], bindings);
-      } else if (/^on[A-Z]*/.test(key)) {
-        setEventBinding(elem, vm, key.slice(2).toLowerCase(), elem.dataset[key]);
-      } else {
-        setOnewayBinding(elem, vm, key === 'bind' ? 'textContent' : key, elem.dataset[key], bindings);
-      }
-    })
-  });
-  
-  return startWatching(vm, bindings);
-}
+window.bootstrap = function bootstrap(root, vm) {
+  return new Binder(root, vm).viewModel;
+};
